8832f0924dd7d2e64c1569946d2a8a5a4c8a4b54
jonckheerethijs <jonckheerethijs@gmail.com>
2023-09-23 13:58:08 +0200

    first version commit before i destroy

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let elementsToRemove = arguments.length
+    for (i=1;i<elementsToRemove;i++){
+        if (array.length >= arguments[i]){
+            console.log(`Remove element on position ${arguments[i]}`)
+            console.log(`The element to be removed is ${array[arguments[i]-1]}`)
+            array.splice(arguments[i]-1,1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
