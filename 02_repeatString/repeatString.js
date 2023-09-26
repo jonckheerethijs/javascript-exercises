d6cfae616dc641d3cbf92371b19e6e51a05b75cc
jonckheerethijs <jonckheerethijs@gmail.com>
2023-09-26 21:03:40 +0200

    solve problems numbered 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    if (repeats<0){
+        return "ERROR"
+    }
+    result="";
+    for (let i = 0; i<repeats; i++){
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    let result = 0
+    if (typeof(first)!="number"||typeof(second)!="number"||first<0||second<0){
+        return 'ERROR'
+    }
+    if (second < first){
+        let x = second
+        second = first
+        first = x
+    }
+    for (let i = first;i<=second;i++){
+        result += i
+    }
+    console.log(result)
+    return result
 };
 
 // Do not edit below this line
